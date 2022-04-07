82853f9704454f39984b99cda297514cd7f9ac0d
Sebastián Castaño <contacto@weremote.net>
2022-04-07 17:03:23 -0500

    Add new files

---
+++ cmd/tfidf/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	/*
+		TODO: write your code here
+		Please follow the instructions in the README.md file.
+		You're free to use external packages as long as it
+		doesn't solves the proposed problems directly.
+
+		Be creative and have fun! :)
+	*/
+}
